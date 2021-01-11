ee0bb99f5160a8c0c4be4eaa282ce298dc4464a2
PolishukArtem <CazeOne1@gmail.com>
2021-01-11 19:30:47 +0200

    Fill lamp

--- interf/src/Lamp.java
+++ interf/src/Lamp.java
@@ -1,3 +1,11 @@
 class Lamp implements LightSource {
-
+    int lightPower;
+    Lamp(int lightPower) {
+        this.lightPower = lightPower;
+    }
+    public int getLightPower() { return lightPower; }
+    @Override
+    public void light() {
+        System.out.println("Lamp on the ceiling lights with light power: " + lightPower);
+    }
 }
