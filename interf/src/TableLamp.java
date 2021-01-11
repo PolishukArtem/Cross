249b261cfb8ce5aa97989e70adf5f6eb990c74ab
PolishukArtem <CazeOne1@gmail.com>
2021-01-11 19:32:08 +0200

    Filled light sourse and Table lamp

--- interf/src/TableLamp.java
+++ interf/src/TableLamp.java
@@ -1,3 +1,11 @@
 public class TableLamp extends Lamp {
-
+    private String direction;
+    TableLamp(int lightPower, String direction) {
+        super(lightPower);
+        this.direction = direction;
+    }
+    @Override
+    public void light() {
+        System.out.println("Table lamp pointed on " + direction + " with light power: " + lightPower);
+    }
 }
